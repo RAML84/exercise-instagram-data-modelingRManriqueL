f68286f5f5b4d8535329476787a4b41fd1703dc5
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-07-13 11:03:26 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -5,52 +5,52 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
-    fistname = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-class Followers(Base):
-    __tablename__ = 'Followers'
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('User.id'), nullable=False)
-    user_to_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False) 
+
 
 class Post(Base):
-    __tablename__ = 'Post'
-    # Here we define columns for the table address.
+    __tablename__ = 'post'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    User_id = Column(Integer, ForeignKey('User.id'), nullable=(False))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
 
 class Comment(Base):
-    __tablename__ = 'Comment'
-    # Here we define columns for the table address.
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
-    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
-    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
 
 class Media(Base):
-    __tablename__ = "media"
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     type = Column(String(250), nullable=False)
     url = Column(String(250), nullable=False)
-    post_id = Column(Integer, ForeignKey("post_id"), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
 
     def to_dict(self):
         return {}
